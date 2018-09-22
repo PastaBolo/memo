3b0e1b7d9f3e17288eb4d6b0517517c2f494d342
Alain P <alain.paolini291@gmail.com>
2018-09-22 17:50:41 +0200

    #14 Création tag card component seulement pour affichage du tag

---
+++ src/app/shared/tag-card/tag-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'memo-tag-card',
+  templateUrl: './tag-card.component.html',
+  styleUrls: ['./tag-card.component.scss']
+})
+export class TagCardComponent {}
